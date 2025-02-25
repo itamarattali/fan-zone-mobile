7a69166c8e6250940811166d33e5ae1a27e6ec56
DorTKOphir <dor.ophir5@gmail.com>
2025-02-25 21:41:51 +0200

    Feat(splash-screen): Add a splash screen, fix auth bugs

--- app/src/main/java/com/example/fan_zone/MainActivity.kt
+++ app/src/main/java/com/example/fan_zone/MainActivity.kt
@@ -7,13 +7,11 @@ import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.fan_zone.databinding.ActivityMainBinding
 import com.google.android.material.appbar.MaterialToolbar
-import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var navController: NavController
-    private lateinit var firebaseAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,19 +19,12 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        firebaseAuth = FirebaseAuth.getInstance()
-
         val toolbar = findViewById<MaterialToolbar>(R.id.toolbar)
         setSupportActionBar(toolbar)
 
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         navController = navHostFragment.navController
-
-        // Redirect based on authentication status
-        if (firebaseAuth.currentUser != null) {
-            navController.navigate(R.id.profileFragment)
-        }
 
         setupActionBarWithNavController(navController)
     }
