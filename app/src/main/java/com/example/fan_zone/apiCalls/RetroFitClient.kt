74a3fd709a5b356c7599e6527e5299827f154a00
itamarattali <73632092+itamarattali@users.noreply.github.com>
2025-03-05 21:56:10 +0200

    Revert "Revert 34 7 sign in to matches feed flow"

---
+++ app/src/main/java/com/example/fan_zone/apiCalls/RetroFitClient.kt
@@ -0,0 +1,27 @@
+package com.example.fan_zone.apiCalls
+
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.scalars.ScalarsConverterFactory
+
+object RetrofitClient {
+    private const val BASE_URL = "https://api.football-data.org/v4/"
+
+    private val loggingInterceptor = HttpLoggingInterceptor().apply {
+        level = HttpLoggingInterceptor.Level.BODY // Logs full request & response
+    }
+
+    private val client = OkHttpClient.Builder()
+        .addInterceptor(loggingInterceptor) // Attach interceptor
+        .build()
+
+    val instance: FootballApiService by lazy {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(client) // Attach client with logging
+            .addConverterFactory(ScalarsConverterFactory.create())
+            .build()
+            .create(FootballApiService::class.java)
+    }
+}
