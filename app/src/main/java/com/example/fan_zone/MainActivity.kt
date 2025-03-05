b2227151b4e855c4726c87baf4fe8985ed9a0140
DorTKOphir <dor.ophir5@gmail.com>
2025-03-05 21:39:14 +0200

    Feat(navigation-flow): Add BottomNavigationView to MainActivity and implement working navigation between matches feed and profile

--- app/src/main/java/com/example/fan_zone/MainActivity.kt
+++ app/src/main/java/com/example/fan_zone/MainActivity.kt
@@ -2,9 +2,12 @@ package com.example.fan_zone
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.example.fan_zone.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,5 +15,13 @@ class MainActivity : AppCompatActivity() {
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.mainNavHostFragment) as? NavHostFragment
+        val navController = navHostFragment?.navController
+
+        if (navController != null) {
+            binding.bottomNavigationView.setupWithNavController(navController)
+        }
     }
 }
