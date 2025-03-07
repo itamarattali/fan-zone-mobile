78871cdbfa3b5b7ef46fe8b783610d372cff8cee
idan janach <idanjanach4455@gmail.com>
2025-03-07 12:41:02 +0200

    replaced observeforever and cleaned code

--- app/src/main/java/com/example/fan_zone/viewModels/MatchListViewModel.kt
+++ app/src/main/java/com/example/fan_zone/viewModels/MatchListViewModel.kt
@@ -2,10 +2,10 @@ package com.example.fan_zone.viewModels
 
 
 import android.app.Application
-import android.util.Log
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.switchMap
 import com.example.fan_zone.models.Match
 import com.example.fan_zone.repositories.MatchRepository
 import java.util.Calendar
@@ -14,29 +14,18 @@ import java.util.Date
 class MatchListViewModel(application: Application) : AndroidViewModel(application) {
 
     private val matchRepository = MatchRepository(application)
-    private val _matches = MutableLiveData<List<Match>>()
-    val matches: LiveData<List<Match>> get() = _matches
 
     private val _selectedDate = MutableLiveData<Date>()
-    val selectedDate: LiveData<Date> get() = _selectedDate
+
+    val matches: LiveData<List<Match>> get() = _selectedDate.switchMap { date: Date -> matchRepository.getMatchesByDate(date) }
 
     init {
         val today = Calendar.getInstance().time
         _selectedDate.value = today
-        loadMatchesForDate(today)
     }
 
     fun loadMatchesForDate(date: Date) {
         _selectedDate.value = date
-        selectedDate.value?.let {
-            matchRepository.getMatchesByDate(it).observeForever {
-                _matches.value = it
-            }
-        }
     }
 
-    override fun onCleared() {
-        super.onCleared()
-        selectedDate.value?.let { matchRepository.getMatchesByDate(it).removeObserver { } }
-    }
 }
