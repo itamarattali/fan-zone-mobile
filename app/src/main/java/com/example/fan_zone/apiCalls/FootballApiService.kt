3bf48ad7913bcff2b9668c903df9c7cc8a39262b
itamarattali <itamarattali@gmail.com>
2025-03-29 17:10:21 +0300

    fix (local.properties) fix matches api key not working

--- app/src/main/java/com/example/fan_zone/apiCalls/FootballApiService.kt
+++ app/src/main/java/com/example/fan_zone/apiCalls/FootballApiService.kt
@@ -2,17 +2,12 @@ package com.example.fan_zone.apiCalls
 
 import retrofit2.Call
 import retrofit2.http.GET
-import retrofit2.http.Headers
 import retrofit2.http.Query
-import com.example.fan_zone.BuildConfig
-
-const val apiKey = BuildConfig.MATCHES_API_KEY
 
 interface FootballApiService {
-    @Headers("X-Auth-Token: $apiKey")
     @GET("competitions/PL/matches")
     fun getMatches(
         @Query("dateFrom") dateFrom: String,
         @Query("dateTo") dateTo: String
-    ): Call<String> // Changed to Call<String> for raw JSON
+    ): Call<String>
 }
